4e626117cddc888384f5048b24849edec596c486
msuser1 <msuser1@msws3.(none)>
2010-09-08 20:42:36 +0530

    minor fix for test script

--- ops/integration_testing.sh
+++ ops/integration_testing.sh
@@ -4,5 +4,5 @@ export ORIGINAL_PATH=$PATH
 export MY_RUBY_HOME=~/.ruby_versions/ruby-1.8.6-p383
 export PATH=$GEM_HOME/bin:$MY_RUBY_HOME/bin:$ORIGINAL_PATH
 script/server&
-cd ../cucumber_testing
+cd ../../cucumber_testing
 cucumber  --tags @scout
