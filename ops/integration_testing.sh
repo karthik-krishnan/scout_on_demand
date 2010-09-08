79c27321d7792730bf27dbf793bb56ab437dece7
msuser1 <msuser1@msws3.(none)>
2010-09-08 19:18:45 +0530

    integration testing.sh updated with cucumber comminad

--- ops/integration_testing.sh
+++ ops/integration_testing.sh
@@ -5,4 +5,4 @@ export MY_RUBY_HOME=~/.ruby_versions/ruby-1.8.6-p383
 export PATH=$GEM_HOME/bin:$MY_RUBY_HOME/bin:$ORIGINAL_PATH
 script/server
 cd ../../cucumber_testing
-cucumber features/manage_scout_inbox_users.feature
+cucumber  --tags @scout
