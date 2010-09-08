bb69eff22dc7ee5523966622ee5ea2a19f70ce9e
msuser1 <msuser1@msws6.(none)>
2010-09-08 17:40:30 +0530

    spec is updated

--- ops/spec/models/message_spec.rb
+++ ops/spec/models/message_spec.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Message do
   before(:each) do
     @m = Message.new
+    @m.mail_to = "john@scout.com"
   end
 
   it do
