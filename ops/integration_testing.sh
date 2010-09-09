492d8cb85525575330efd49f3c29e5d767790d72
msuser1 <msuser1@msws1.(none)>
2010-09-09 16:11:29 +0530

    hardcoded results.html path removed

--- ops/integration_testing.sh
+++ ops/integration_testing.sh
@@ -4,6 +4,7 @@ export ORIGINAL_PATH=$PATH
 export MY_RUBY_HOME=~/.ruby_versions/ruby-1.8.6-p383
 export PATH=$GEM_HOME/bin:$MY_RUBY_HOME/bin:$ORIGINAL_PATH
 script/server&
+result_path=$PWD
 cd /home/msuser1/workspace/cucumber_testing/
 cucumber  --tags @scout --format html --out cucumber_report.html
-cp cucumber_report.html "/home/msuser1/hudson/jobs/Cucumber Functional Test/workspace/ops/doc/results.html"
+cp cucumber_report.html "$result_path/doc/results.html"
